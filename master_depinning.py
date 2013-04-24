1035b12bd1d7e13c072ce6ebae76d9c353623673
unknown <ppxdep@pppzawr01.ad.nottingham.ac.uk>
2013-04-24 13:46:10 +0100

    Make screen output more verbose.

--- master_depinning.py
+++ master_depinning.py
@@ -7,14 +7,17 @@ import subprocess
 import os
 
 # Convert odt files to .dat with odt2dat.py
+print "Convert data table to .dat format"
 subprocess.call("odt2dat.py", shell=True)
 
 	
 # Convert omf files to bmp with avf2bmp.py *.bmp
+print "Convert omf vector files to bitmaps"
 subprocess.call("avf2bmp.py *.omf", shell=True)
 
 # change to the bmp directory
 os.chdir('../bmp')
 	
 # convert bmpfiles to avi with bmp2avi.py
+print "Convert bitmap images to a video"
 subprocess.call("bmp2avi.py", shell=True)
