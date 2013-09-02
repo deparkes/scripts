fc03c7550fdd4633a15b824e311e875910dc8aa3
Duncan Parkes <ppxdep@nottingham.ac.uk>
2013-09-02 17:30:35 +0100

    Remove bmp files as they take up so much room

--- master_depinning.py
+++ master_depinning.py
@@ -7,6 +7,19 @@
 # - Also include generation of matlab files
 import subprocess
 import os
+import glob
+
+def get_bmp(path):
+    # A function to find the omf magnetisation vector files in a particular folder.
+    # Since I only want one I have an if statement to try and protect things. Needs improvements.
+    # Use os.path.basename(path) to strip away everything but the file name from the path.
+    # print os.getcwd()
+    # os.chdir('../output/15um90umBarWithCrosses.bmp/strain_0');
+    omf_path = '%s/*.bmp' % (path)
+    #create an array of file names based on the search for omf files.
+    files_array = glob.glob(omf_path)
+    return files_array
+
 
 # Convert odt files to .dat with odt2dat.py
 print "Convert data table to .dat format"
@@ -22,3 +35,9 @@ os.chdir('../bmp')
 # convert bmpfiles to avi with bmp2avi.py
 print "Convert bitmap images to a video"
 subprocess.call("bmp2avi.py", shell=True)
+
+# Delete bmp files as they take up so much room.
+print "Remove bmp files as they take up so much room"
+current_dir = os.getcwd()
+for i, filename in enumerate(get_bmp(current_dir)): # loop through each file
+    os.remove(filename)
